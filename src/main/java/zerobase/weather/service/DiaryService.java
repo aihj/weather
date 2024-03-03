1a524d18770f152c586d523485f23562f9693b40
Heejae Kim <heejae9102@gmail.com>
2024-03-03 22:59:53 +0900

    init

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -4,11 +4,14 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.WeatherApplication;
 import zerobase.weather.domain.DateWeather;
 import zerobase.weather.domain.Diary;
 import zerobase.weather.repository.DateWeatherRepository;
@@ -35,6 +38,8 @@ public class DiaryService {
 
     private final DateWeatherRepository dateWeatherRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(WeatherApplication.class);
+
     public DiaryService(DiaryRepostiory diaryRepostiory, DateWeatherRepository dateWeatherRepository) {
         this.diaryRepostiory = diaryRepostiory;
         this.dateWeatherRepository = dateWeatherRepository;
@@ -47,7 +52,9 @@ public class DiaryService {
     }
 
     @Transactional(isolation = Isolation.SERIALIZABLE)
+
     public void createDiary(LocalDate date, String text){
+        logger.info("started to create diary");
         // open weather map에서 날씨 데이터 가져오기
         DateWeather dateWeather = getDateWeather(date);
 
@@ -57,6 +64,7 @@ public class DiaryService {
         nowDiary.setText(text);
         nowDiary.setDate(date);
         diaryRepostiory.save(nowDiary);
+        logger.info("end to create diary");
 
     }
 
@@ -92,8 +100,8 @@ public class DiaryService {
 
     @Transactional(readOnly = true)
     public List<Diary> readDiaries(LocalDate startDate, LocalDate endDate){
-
-    return diaryRepostiory.findAllByDateBetween(startDate, endDate);
+        logger.debug("read diary");
+        return diaryRepostiory.findAllByDateBetween(startDate, endDate);
     }
 
     public void updateDiary(LocalDate date, String text){
